a9c7d5efd4713130f3205f362bdc08f3e8d1176c
smartramana <smartramana@gmail.com>
2023-09-11 15:12:51 -0400

    Update instance.tf

--- module/instance.tf
+++ module/instance.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "ec2" {
     volume_type           = var.root_volume_type
     volume_size           = var.root_volume_size
     delete_on_termination = true
-    encrypted = true
+    encrypted = false
   }
 
   tags = local.tags
